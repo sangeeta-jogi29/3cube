3c7141b09265b684521fdce70378569c56667198
sangeeta-jogi29 <sangeeta.jogi29@gmail.com>
2025-06-16 17:28:30 +0530

    Set base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
+  base: '/3cube/', // 👈 REQUIRED for GitHub Pages
+  plugins: [react(), tailwindcss()],
 })
