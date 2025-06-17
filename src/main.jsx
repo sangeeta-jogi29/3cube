71ac69b8ce32028431c92385bf207ca2ba83ec17
sangeeta-jogi29 <sangeeta.jogi29@gmail.com>
2025-06-17 15:48:21 +0530

    main upadted

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import './styles/Wellness.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // 👈 Use HashRouter
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
+
 
